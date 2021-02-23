7dfc497342a60652996e65a4f93187f211a75fc7
sziccardi1998 <sziccardifullstack@gmail.com>
2021-02-22 21:29:47 -0500

    Change to api route.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ var db = require("../models/workout");
 
 module.exports = function(app) {
     app.get("/api/workouts", function(req, res) {
-        db.Workout.find({}).then(result => {
+        db.find({}).then(result => {
             res.json(result);
         })
         .catch(err => {
@@ -11,7 +11,7 @@ module.exports = function(app) {
     });
 
     app.put("/api/workouts/:id", function({body, params}, res) {
-        db.Workout.findByIdAndUpdate(
+        db.findByIdAndUpdate(
             params.id,
             { $push: { exercises: body } },
             { new: true }
@@ -23,7 +23,7 @@ module.exports = function(app) {
     });
 
     app.post("/api/workouts", function (req, res) {
-        db.Workout.create({}).then(result => {
+        db.create({}).then(result => {
             res.json(result);
         }).catch(err => {
             res.json(err);
@@ -31,7 +31,7 @@ module.exports = function(app) {
     });
 
     app.get("/api/workouts/range", function(req, res) {
-        db.Workout.find({}).limit(5).then(result => {
+        db.find({}).limit(5).then(result => {
             res.json(result);
         }).catch(err => {
             res.json(err);
